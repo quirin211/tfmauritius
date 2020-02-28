ccdeb92c63e4cb86cce11719d2b902d31d2a46a2
flmarins <flmarins@gmail.com>
2020-02-28 06:52:14 +0100

    Update main.tf

--- exercice-5/main.tf
+++ exercice-5/main.tf
@@ -1,4 +1,6 @@
-provider "azurerm" {}
+provider "azurerm" {
+    version = "~> 1.x"
+  }
 
 resource "azurerm_resource_group" "rg_coding_dojo" {
   name     = "rg_${terraform.workspace}_coding_dojo"
